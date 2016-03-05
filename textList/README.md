e7f46e6a6b2537c7b565813a2d7a8a119ed10e05
Stephen Printup <stephen.printup@gmail.com>
2016-03-05 16:35:00 -0700

    textList

---
+++ textList/README.md
@@ -0,0 +1,10 @@
+I followed this [tutorial](http://portlandcodeschool.github.io/jse/2015/09/16/sep16-backbone2/) and read the [docs](http://backbonejs.org/#Getting-started).
+
+Things I learned include: 
+-[$el](http://backbonejs.org/#View-$el)
+-backbone [mv](http://backbonejs.org/#Model-View-separation)-[c](http://backbonejs.org/#Model-Collections)
+-css element [keycodes](https://css-tricks.com/snippets/javascript/javascript-keycodes/)
+-[event handlers](http://idratherbewriting.com/events-and-listeners-javascript/)
+-$(document).ready( function () {}): to be run only when the document is ready as per jQuery
+-jQuery [manipulation](http://api.jquery.com/category/manipulation/) using setters like .append()
+-[CSS psuedo-classes](http://www.w3schools.com/css/css_pseudo_classes.asp)
---
+++ textList/textlist.js
@@ -0,0 +1,86 @@
+$(document).ready( function () {
+
+    var TextModel = Backbone.Model.extend({
+        defaults : {"value" : ""},
+        replace : function (str) {
+          this.set("value", str);                               //Is this the model or the element? element
+        }
+    });
+
+    var TextView = Backbone.View.extend({
+        render: function () {
+            var textVal = this.model.get("value");
+            var btn = '<button>Clear</button>';
+            var dbtn = '<button id=\'delete\'>Delete</button>';
+            var input = '<input type="text" value="' + textVal + '" />';
+            this.$el.html(textVal+"<br><div>" + input + btn + dbtn +"</div>");
+        },
+        initialize: function () {
+            this.model.on("change", this.render, this);
+            // last argument 'this' ensures that render's
+            // 'this' means the view, not the model
+        },
+        events : {
+            "click button" : "clear",
+            "keypress input" : "updateOnEnter",                  //Why are the event keys so loosly defined? 
+            "click #delete"    : "deleteModel"
+        },
+        replace : function () {
+            var str = this.$el.find("input").val();
+            this.model.replace(str);
+        },
+        clear: function () {
+            this.model.replace("");
+        },
+        updateOnEnter: function (e){
+            if(e.keyCode == 13) {
+                this.replace();
+            }
+        },
+        deleteModel : function (argument) {
+            // body...
+            console.log("delete button pressed with argument: " + argument + 'and this: ' + this);
+            this.remove();
+        }
+    });
+
+    var TextCollection = Backbone.Collection.extend({
+        model : TextModel
+    });
+
+    var TextCollectionView = Backbone.View.extend({
+        render : function () {
+            var btn = '<button id="addbutton">Add Text</button>';
+            var div = '<div id="text-list"></div>';
+            this.$el.html(div + btn);
+        },
+        initialize : function () {
+            this.listenTo(this.collection, 'add', this.addView);
+        },
+        events : {
+            "click #addbutton" : "addModel"
+        },
+        addModel : function () {
+            this.collection.add({});
+            // collection adds a model, fires add event, then listener calls this.addView(model)
+        },
+        addView : function (newModel) {
+            newModel.set("value","Enter something here...");
+            var view = new TextView({model : newModel});
+            view.render();
+            this.$("#text-list").append(view.$el);
+        }
+    });
+
+    var textCollection = new TextCollection();
+
+    var textCollectionView = new TextCollectionView({ collection : textCollection});
+
+    textCollectionView.render();
+
+    $("#listdiv").append(textCollectionView.$el);               
+    /*
+        why does textCollectionView have a cached jQuery object?
+    */
+
+});
