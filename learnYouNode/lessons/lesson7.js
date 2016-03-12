a5be9317c631c9215b78e4870ffe626c00294ec0
Stephen Printup <stephen.printup@gmail.com>
2016-03-12 06:26:12 -0700

    learnyounode lesson 9

--- learnYouNode/lessons/lesson7.js
+++ learnYouNode/lessons/lesson7.js
@@ -1,3 +1,11 @@
+/*
+ ## HTTP CLIENT (Exercise 7 of 13)  
+   
+  Write a program that performs an HTTP GET request to a URL provided to you  
+  as the first command-line argument. Write the String contents of each  
+  "data" event from the response to a new line on the console (stdout).
+*/
+
 var http = require('http');
 
 http.get(process.argv[2], function callback (response) {
